7a9a2d333f1a83844dc88df9b3a0b7555aafede6
gabriel <lb.j.23@hotmail.com>
2021-01-20 14:15:48 -0400

    Chat finalizado v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,7 +1,5 @@
 var socket = io();
 
-//LEER POR PARAMETRO LO QUE SEA QUE VENGA CON EL NOMBRE DEL USUARIO Y ASI NO SE DUPLIQUEN
-
 var params = new URLSearchParams(window.location.search);
 
 if (!params.has('nombre') || !params.has('sala')) {
@@ -14,14 +12,15 @@ var usuario = {
     sala: params.get('sala')
 };
 
+
+
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function(resp) {
-        console.log('Usuarios conectados', resp);
-
-        return resp;
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -32,29 +31,24 @@ socket.on('disconnect', function() {
 });
 
 
-// Enviar información
-//socket.emit('crearMensaje', {
-//nombre: 'Gabriel',
-// mensaje: 'Welcome to Node JS'
-//}, function(resp) {
-//console.log('respuesta server: ', resp);
-//});
+
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-    console.log('Servidor:', mensaje);
-
+    //console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-//ESCUCHAR CAMBIOS DE USUARIOS
-//CUANDO UN USUARIO ENTRA O SALE DEL CHAT
-
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-    console.log(personas);
+    renderizarUsuarios(personas);
 });
 
-//MENSAJES PRIVADOS (escuchar la accion del cliente)
-
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
+
     console.log('Mensaje Privado:', mensaje);
+
 });
